40c7e99fff1086f0832c0afd87d312662271cac9
Srivatsa03 <skamb10@uic.edu>
2026-03-10 18:51:00 -0500

    Updated Files

--- Jenkins-Server-TF/tools-install.sh
+++ Jenkins-Server-TF/tools-install.sh
@@ -7,7 +7,7 @@ sudo apt install openjdk-17-jdk -y
 java --version
 
 # Installing Jenkins
-curl -fsSL https://pkg.jenkins.io/debian/jenkins.io-2023.key | sudo tee \
+curl -fsSL https://pkg.jenkins.io/debian-stable/jenkins.io-2023.key | sudo tee \
   /usr/share/keyrings/jenkins-keyring.asc > /dev/null
 echo deb [signed-by=/usr/share/keyrings/jenkins-keyring.asc] \
   https://pkg.jenkins.io/debian binary/ | sudo tee \
