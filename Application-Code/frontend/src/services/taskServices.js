6298f996c689f60d8bf8e56d07151d417230da07
Srivatsa Kamballa <skamb10@uic.edu>
2026-03-11 15:17:19 -0500

    Update apiUrl to include default AWS endpoint

--- Application-Code/frontend/src/services/taskServices.js
+++ Application-Code/frontend/src/services/taskServices.js
@@ -1,5 +1,5 @@
 import axios from "axios";
-const apiUrl = process.env.REACT_APP_BACKEND_URL //"http://localhost:8080/api/tasks";
+const apiUrl = process.env.REACT_APP_BACKEND_URL || "http://k8s-threetie-mainlb-b5b9250791-2093763814.us-east-1.elb.amazonaws.com/api/tasks";
 console.log(apiUrl)
 export function getTasks() {
     return axios.get(apiUrl);
